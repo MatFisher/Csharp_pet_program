38de285550af3c0f1a1de327eb378c7d0de794eb
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:41:56 +0300

    Перевод числа из десятичного в двоичное

---
+++ Language_test_task/042_From10to2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+int ConvertToTwo(int number)
+{
+    int twoNumber = 0;
+    for (int i = 0; number != 0; i++)
+    {
+        twoNumber = twoNumber + (number % 2) * Convert.ToInt32(Math.Pow(10, i));
+        number /= 2;
+    }
+    return twoNumber;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {num} in10 = {ConvertToTwo(num)} in2");
+Console.ReadKey();
