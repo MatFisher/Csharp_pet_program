f5094a46f364384956606a2258ded577b01ce1e4
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:44:32 +0300

    Вывод последовательности Фибоначчи

---
+++ Language_test_task/044_Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать числа Фибоначчи
+
+int[] Fibbonah(int number)
+{
+    int[] array = new int[number];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+Console.WriteLine("Какое количество чисел показать?");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = Fibbonah(num);
+Console.Write($"{num} чисел Фибоначчи -> ");
+PrintArray(arr);
+Console.ReadKey();
