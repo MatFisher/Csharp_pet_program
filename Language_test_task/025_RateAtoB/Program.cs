3741d62550527ad3626ee85961abe1d933ab2cce
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:07:41 +0300

    Возведение числа А в степень B (цикл)

---
+++ Language_test_task/025_RateAtoB/Program.cs
@@ -0,0 +1,19 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponent(int numA, int numB)
+{
+    int numHelp = numA;
+    for (int index = 1; index < numB; index++)
+    {
+        numA = numA * numHelp;
+    }
+    return numA;
+}
+int exp = Exponent(numberA, numberB);
+Console.WriteLine($"Результат = {exp}");
+Console.ReadKey();
