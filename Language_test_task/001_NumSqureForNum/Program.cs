66d49544a6e62f2e32657d8c98df1444d02e842d
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:25:25 +0300

    Проверка, является ли число квадратом другого

---
+++ Language_test_task/001_NumSqureForNum/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число");
+        int numA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        int numB = Convert.ToInt32(Console.ReadLine());
+
+        if (numA > numB && 1 == numA / (numB * numB)) Console.WriteLine("Квадратом является");
+        else if (numB > numA && 1 == numB / (numA * numA)) Console.WriteLine("Квадратом является");
+        else
+        {
+            Console.WriteLine("Квадратом не является");
+        }
+        Console.ReadKey();
+    }
+}
