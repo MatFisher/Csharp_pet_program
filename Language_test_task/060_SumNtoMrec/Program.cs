0f43df78237c368a7e7aaed46523a435a2f95f5d
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 18:09:32 +0300

    Сумма диапазона чисел (рекурсия)

---
+++ Language_test_task/060_SumNtoMrec/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int GetSumNumbers(int num1, int num2, int sum = 0)
+{
+    if (num1 == num2) return sum += num1;
+    else
+    {
+        sum += num1;
+        return GetSumNumbers(++num1, num2, sum);
+    }
+}
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = GetSumNumbers(numberA, numberB);
+Console.WriteLine($"Сумма числе между {numberA} и {numberB} = {sum}");
+Console.ReadKey();
