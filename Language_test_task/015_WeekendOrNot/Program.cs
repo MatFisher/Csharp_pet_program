b679d073d45cb35593abf5c34f7155ab362aae9d
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:55:56 +0300

    Является ли номер дня недели выходным

---
+++ Language_test_task/015_WeekendOrNot/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 5 && number < 8) Console.WriteLine("Выходной");
+else Console.WriteLine("Не выходной");
+Console.ReadKey();  
