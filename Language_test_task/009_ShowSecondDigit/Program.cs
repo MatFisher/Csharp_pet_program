c7db03927119905e345df83dbcc77c51f3409e73
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:17:39 +0300

    Показать вторую цифру числа

---
+++ Language_test_task/009_ShowSecondDigit/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = (number % 100) / 10;
+    Console.Write($"Вторая цифра = {secondDigit}");
+}
+else Console.WriteLine("Введите корректное число");
+Console.ReadKey();
