dc05db2695d47c4330fb05c621c3137c7a6697f6
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:09:38 +0300

    Определение суммы цифр в числе

---
+++ Language_test_task/027_SumDigitNumb/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitNumb(int num)
+{
+    int sum = 0;
+    while (num % 10 > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
+int summ = SumDigitNumb(number);
+Console.WriteLine($"Сумма цифр в числе = {summ}");
+
+Console.ReadKey();
