7b0773da83040766220ba044afb12b75d006126b
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:31:24 +0300

    Показать числа от -N до N

---
+++ Language_test_task/006_ShowNumb-NtoN/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) for (int count = -number; count <= number; count++) Console.Write($"{count} ");
+else Console.WriteLine("Введено не натуральное число");
+Console.ReadKey();
