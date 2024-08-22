aa63311a6b540e95784dd403691ef38ae4b8ebc8
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 18:10:22 +0300

    Сумма цифр в числе (рекурсия)

---
+++ Language_test_task/061_SumDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigits(number);
+Console.WriteLine($"Сумма цифр равна {result} ");
+Console.ReadKey();
