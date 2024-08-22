2bce316ec0a025e7594c9551337d479c92a46ec5
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:24:12 +0300

    Вывести квадрат числа

---
+++ Language_test_task/000_NumSquare/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+number *= number;
+Console.Write($"Квадрат числа = {number}");
+Console.ReadKey();
---
+++ Language_test_task/000_NumSquare/ProgramBase.cs
@@ -0,0 +1,9 @@
+﻿internal class ProgramBase
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Квадрат числа = {number *= number}");
+    }
+}
