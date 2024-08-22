ffcac35c8aeacc6a5e59f3a87c7a782bf2ac45f9
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:16:13 +0300

    Показать числа от 1 до N

---
+++ Language_test_task/007_ShowEvenNumb1toN/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) for (int count = 2; count <= number; count += 2) Console.Write($"{count} ");
+else Console.WriteLine("Введено не натуральное число");
+Console.ReadKey();
