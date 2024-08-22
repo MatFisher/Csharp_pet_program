7148beafba3f8fb3bf7ed3551a3cdd9930fb9ae0
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:40:03 +0300

    Могут ли числа быть сторонами треугольника

---
+++ Language_test_task/040_3NumbOfTriang/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить являются ли три числа сторонами треугольника 
+
+Console.WriteLine("Введите сторону А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+string Pifagor(int a, int b, int c)
+{
+    if (c < a+b && a < c+b && b < a+c) return "Да";
+    else return "Нет";
+}
+
+string result = Pifagor(numA, numB, numC);
+Console.WriteLine(result);
+Console.ReadKey();
