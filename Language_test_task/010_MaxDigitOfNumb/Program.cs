99dcb6eea46a8d2d749d2e0000bfc58130aea06a
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:18:18 +0300

    Показать максимальную цифру в числе

---
+++ Language_test_task/010_MaxDigitOfNumb/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 -99 = {number}");
+
+int MaxDigit(int num) //
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа = {maxDigit}");
+Console.ReadKey();
