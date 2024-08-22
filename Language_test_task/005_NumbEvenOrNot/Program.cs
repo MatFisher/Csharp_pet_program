6e03f0f73f1eb6a095c073074c6ea72b0f1769f6
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:29:19 +0300

    Является ли число чётным

---
+++ Language_test_task/005_NumbEvenOrNot/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA % 2 == 0) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
+Console.ReadKey();
