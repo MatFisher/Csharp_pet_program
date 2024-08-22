ad1a2cdfc1c62c202dcd7ca2fd64c2c4e313181a
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:27:36 +0300

    По номеру дня недели вывести его название

---
+++ Language_test_task/003_NumDayOfWeek/Program.cs
@@ -0,0 +1,14 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) Console.Write("Понедельник");
+else if (number == 2) Console.Write("Вторник");
+else if (number == 3) Console.Write("Среда");
+else if (number == 4) Console.Write("Четверг");
+else if (number == 5) Console.Write("Пятница");
+else if (number == 6) Console.Write("Суббота");
+else if (number == 7) Console.Write("Воскресенье");
+else Console.Write("Неверный номер");
+Console.ReadKey();
