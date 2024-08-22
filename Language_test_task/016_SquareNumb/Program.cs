c77b55107d09647cfdbc9ed18d800ffb12a07799
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:56:57 +0300

    Является ли одно число квадратом другого

---
+++ Language_test_task/016_SquareNumb/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareNumbers(numberA, numberB) ? "является" : "не является");
+
+
+bool SquareNumbers(int numA, int numB)
+{
+    return numA * numA == numB || numB * numB == numA;
+}
+
+Console.ReadKey();
