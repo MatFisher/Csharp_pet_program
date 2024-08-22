6200b990b7dd9ce7b5d06d1f02727f78185c1671
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:10:59 +0300

    Вычисление факториала

---
+++ Language_test_task/028_FactNumb1toN/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("ВВедите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FactNumbers(int numb)
+{
+    int sum = 1;
+    for (int i = 1; i <= numb; i++)
+    {
+        checked
+        {
+            sum = sum * i;
+        }
+    }
+    return sum;
+}
+
+int result = FactNumbers(number);
+Console.WriteLine(result);
+Console.ReadKey();
