e7e39d74da38909e29a723e15202893a997a5e7e
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:59:49 +0300

    Показать диапазоны координат по номеру четверти

---
+++ Language_test_task/018_SquareSet/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти");
+int numberA = Convert.ToInt32(Console.ReadLine());
+string Result(int num)
+{
+    if (num == 1) return "Координаты х > 0, координаты y > 0";
+    if (num == 2) return "Координаты х < 0, координаты y > 0";
+    if (num == 3) return "Координаты х < 0, координаты y < 0";
+    if (num == 4) return "Координаты х > 0, координаты y < 0";
+    return "Введите корректный номер четврети";
+}
+
+string res = Result(numberA);
+Console.WriteLine(res);
+Console.ReadKey();
