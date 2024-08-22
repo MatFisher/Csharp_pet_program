36783a4d131d9f8755369a23f886e42027d62c81
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:08:39 +0300

    Определение количества цифр в числе

---
+++ Language_test_task/026_HowManyDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int HowManyDigit(int num)
+{
+    int index = 0;
+    do
+    {
+        index = index + 1;
+        num = num / 10;
+    } while (num % 10 > 0);
+    return index;
+}
+
+int size = HowManyDigit(number);
+Console.WriteLine($"Количество цифр в числе = {size}");
+Console.ReadKey();
