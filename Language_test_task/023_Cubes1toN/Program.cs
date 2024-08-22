84fbba52f38572826c8d67de35d9433cb37a3337
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:05:23 +0300

    Показать таблицу кубов чисел от 1 до N

---
+++ Language_test_task/023_Cubes1toN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int index = 1;
+    int result = 0;
+    while (index <= num)
+    {
+        result = index * index * index;
+        Console.WriteLine($"{index,5} = {result}");
+        index++;
+    }
+}
+
+Square(number);
+Console.ReadKey();
