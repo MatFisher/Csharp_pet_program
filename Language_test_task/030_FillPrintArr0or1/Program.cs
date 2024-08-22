e286b6c36474b788c7201949006685e26d0d282e
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:13:16 +0300

    Создание массива из нолей и единиц

---
+++ Language_test_task/030_FillPrintArr0or1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(0, 2);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}");
+}
+
+int[] array = FillArray(8);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Console.ReadKey();
