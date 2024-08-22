1de403a294289005c4d2d8904f9a394873661930
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:12:45 +0300

    Задание массива и вывод

---
+++ Language_test_task/029_FillPrintArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+using System.Drawing;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    array[i] = new Random().Next(1, 10);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+     for(int i = 0; i < array.Length; i++)
+        Console.Write($" {array[i]} ");
+}
+
+int[] array = FillArray(8);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Console.ReadKey();
