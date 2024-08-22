9e861ee55e8a4a275ac9a54f3c260b61415c8443
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:26:12 +0300

    Показать большее и меньшее число

---
+++ Language_test_task/002_ShowBigSmallNum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB) Console.WriteLine($"Большее число = {numA}, Меньшее число = {numB}");
+else Console.WriteLine($"Большее число = {numB}, Меньшее число = {numA}");
+Console.ReadKey();
