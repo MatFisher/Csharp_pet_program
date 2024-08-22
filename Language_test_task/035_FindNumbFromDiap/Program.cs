bcbffd3cda8104d22f3a4b2fc2dc22e293c096a5
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:18:07 +0300

    Поиск элементов в массиве из заданного диапазона

---
+++ Language_test_task/035_FindNumbFromDiap/Program.cs
@@ -0,0 +1,44 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 130);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int NumbOfCut(int[] arr)
+{
+    int size = arr.Length;
+    int numb = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 100 && arr[i] > 9) numb += 1;
+    }
+    return numb;
+}
+
+
+int[] array = new int[123];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int result = NumbOfCut(array);
+Console.WriteLine($"Количество чисел из отрезка = {result}");
+Console.ReadKey();
