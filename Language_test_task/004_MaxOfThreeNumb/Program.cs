f85e961e51ccdeae66686ad33a4e2b37cc80996f
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 15:28:39 +0300

    Наибольшее из трех чисел

---
+++ Language_test_task/004_MaxOfThreeNumb/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+if (max < numB) max = numB;
+if (max < numC) max = numC;
+
+Console.Write($"Максимальное число = {max}");
+Console.ReadKey();
