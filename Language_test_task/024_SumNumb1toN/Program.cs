43a4bfff655d1babcf3afc4e4fc556982951eb3b
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:06:44 +0300

    Показать сумму чисел от 1 до N

---
+++ Language_test_task/024_SumNumb1toN/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("ВВедите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int numb)
+{
+    int sum = 0;
+    for (int i = 1; i <= numb; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+Console.ReadKey();
