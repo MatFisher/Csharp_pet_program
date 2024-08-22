46f63129d85118a29f933567a6cdd7b797d42183
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:18:57 +0300

    Удалить вторую цифру числа

---
+++ Language_test_task/011_DelSecondDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 -999 = {number}");
+
+int DelDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int delDigit = DelDigit(number);
+Console.WriteLine($"Число без второй цифры = {delDigit}");
+Console.ReadKey();
