11bc556aceeb39ed453b4e500fcd18ab3d995f7d
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 16:16:59 +0300

    Показать последнюю цифру числа

---
+++ Language_test_task/008_ShowLastDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = -number;
+if (number >= 100 && number <= 999) Console.WriteLine($"Последняя цифра = {number %= 10}");
+else Console.WriteLine("Введено некорректное число");
+Console.ReadKey();
