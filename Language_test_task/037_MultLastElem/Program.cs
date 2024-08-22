0d1d9c6f03a52c5f633f1b76967f21f12c06eaed
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 17:31:29 +0300

    Перемножение крайних элементов массива

---
+++ Language_test_task/037_MultLastElem/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] CompArray(int[] arr)
+{
+    int size = arr.Length;
+    int[] temp = size % 2 == 0 ? new int[size / 2] : new int[(size / 2) + 1];
+    for (int i = 0; i < temp.Length; i++)
+    {
+        if (i != size - i - 1) temp[i] = arr[i] * arr[size - i - 1];
+        else temp[i] = arr[i];
+    }
+    return temp;
+}
+
+int[] array = CreateArrayRndInt(10, 0, 9);
+PrintArray(array);
+int[] result = CompArray(array);
+PrintArray(result);
+Console.ReadKey();
