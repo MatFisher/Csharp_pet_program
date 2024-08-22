5432c4c02f78bae8711f41f713291213ed1396b8
Матвей Рыбаков <124581249+MatFisher@users.noreply.github.com>
2024-08-22 14:49:10 +0300

    Рекурсия на примере возведения в степень

---
+++ Pet_projects/AsqrBrec/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+int PowNumber(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA * PowNumber(numA, numB - 1);
+}
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = PowNumber(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
+Console.ReadKey();
